f8f41f249721db6d3470ec413bd0005318fb568a
Roman Gershman <romange@gmail.com>
2017-12-17 15:22:24 +0200

    .

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install cmake libunwind8-dev zip flex bison ninja-build autoconf-archive
+apt install -y cmake libunwind8-dev zip flex bison ninja-build autoconf-archive
 
 # for folly & proxygen. gperf is not related to gperftools.
-apt install libboost-all-dev autoconf-archive gperf
+apt install -y libboost-all-dev autoconf-archive gperf
