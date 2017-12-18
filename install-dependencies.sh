4c8ea549e9208e8fc2ae764db94c307755c07d76
Roman Gershman <romange@gmail.com>
2017-12-18 15:50:26 +0200

    More fixes to travis

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y cmake libunwind8-dev zip flex bison ninja-build autoconf-archive
+apt install -y cmake libevent-dev libunwind8-dev zip flex bison ninja-build autoconf-archive
 
 # for folly & proxygen. gperf is not related to gperftools.
 apt install -y libboost-all-dev autoconf-archive gperf
