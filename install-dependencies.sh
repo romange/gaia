7a7522bd2dec2dea65718b58bad3df6eab16d71b
Roman Gershman <romange@gmail.com>
2017-12-25 21:20:47 +0200

    Cache boost directory between builds

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,10 +5,20 @@ apt install -y cmake libevent-dev libunwind8-dev zip flex bison ninja-build auto
 # for folly & proxygen. gperf is not related to gperftools.
 apt install -y autoconf-archive gperf curl
 
-BOOST=boost_1_66_0
-wget http://dl.bintray.com/boostorg/release/1.66.0/source/$BOOST.tar.bz2 \
-    && tar -xjf $BOOST.tar.bz2 && cd $BOOST \
-    && ./bootstrap.sh --prefix=/opt/$BOOST --without-libraries=graph_parallel,graph,wave,test,mpi,python
-./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
-     --without-test -j4
-./b2 install -d0
+BOOST_VER=boost_1_66_0
+
+install_boost() {
+    BOOST=$BOOST_VER
+    wget http://dl.bintray.com/boostorg/release/1.66.0/source/$BOOST.tar.bz2 \
+        && tar -xjf $BOOST.tar.bz2 && cd $BOOST \
+        && ./bootstrap.sh --prefix=/opt/$BOOST --without-libraries=graph_parallel,graph,wave,test,mpi,python
+    ./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
+         --without-test -j4
+    ./b2 install -d0
+}
+
+if ! [ -d /opt/$BOOST_VER ]; then
+  install_boost
+fi
+
+
