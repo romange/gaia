a368dbc4606e06c941f5b563e60a06a5952a336a
Roman Gershman <romange@gmail.com>
2017-12-31 17:02:30 +0200

    .

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ BOOST_VER=boost_1_66_0
 
 install_boost() {
     BOOST=$BOOST_VER
-    wget http://dl.bintray.com/boostorg/release/1.66.0/source/$BOOST.tar.bz2 \
+    wget -nv http://dl.bintray.com/boostorg/release/1.66.0/source/$BOOST.tar.bz2 \
         && tar -xjf $BOOST.tar.bz2 && cd $BOOST \
         && ./bootstrap.sh --prefix=/opt/$BOOST --without-libraries=graph_parallel,graph,wave,test,mpi,python
     ./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
