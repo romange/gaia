eb98162f5e6250af64fda0d5501d6a6f521d2eab
Roman Gershman <romange@gmail.com>
2018-05-12 01:21:21 +0300

    Update boost version

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,15 +7,16 @@ apt install -y cmake libevent-dev libunwind-dev zip flex bison ninja-build autoc
 # for folly & proxygen. gperf is not related to gperftools.
 apt install -y autoconf-archive gperf curl
 
-BOOST_VER=boost_1_66_0
+BOOST_VER=boost_1_67_0
 
 install_boost() {
     BOOST=$BOOST_VER
-    wget -nv http://dl.bintray.com/boostorg/release/1.66.0/source/$BOOST.tar.bz2 \
-        && tar -xjf $BOOST.tar.bz2 && cd $BOOST \
-        && ./bootstrap.sh --prefix=/opt/$BOOST --without-libraries=graph_parallel,graph,wave,test,mpi,python
+    wget -nv http://dl.bintray.com/boostorg/release/1.67.0/source/$BOOST.tar.bz2 \
+        && tar -xjf $BOOST.tar.bz2
+
+    cd $BOOST && ./bootstrap.sh --prefix=/opt/$BOOST --without-libraries=graph_parallel,graph,wave,test,mpi,python
     ./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
-         --without-test -j4
+         --without-test --without-python -j4
     ./b2 install -d0
 }
 
