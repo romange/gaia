aee2d7e40bdbc2aec3a563a11a2c279304718c9f
Roman Gershman <romange@gmail.com>
2018-07-10 15:57:13 +0300

    .

--- examples/asio_fibers.cc
+++ examples/asio_fibers.cc
@@ -62,43 +62,45 @@ class round_robin : public fibers::algo::algorithm {
   std::shared_ptr< asio::io_service >      io_svc_;
   asio::steady_timer                       suspend_timer_;
 //]
-    fibers::scheduler::ready_queue_type      rqueue_;
-    fibers::mutex                            mtx_;
-    fibers::condition_variable               cnd_;
-    std::size_t                                     counter_{ 0 };
+  fibers::scheduler::ready_queue_type      rqueue_;
+  fibers::mutex                            mtx_;
+  fibers::condition_variable               cnd_;
+  std::size_t                                     counter_{ 0 };
 
  public:
   // [asio_rr_service_top
-  struct service : public asio::io_service::service {
-    static asio::io_service::id                  id;
+  struct rr_service : public asio::io_context::service {
 
-    std::unique_ptr< asio::io_service::work >    work_;
+    // Required by add_service.
+    static asio::execution_context::id  id;
 
-    service(asio::io_service& io_svc) : asio::io_service::service(io_svc),
-      work_{ new asio::io_service::work( io_svc) } {
+    // std::unique_ptr< asio::io_service::work >    work_;
+    asio::executor_work_guard<asio::io_context::executor_type> work_;
+
+    rr_service(asio::io_context& io_svc) : asio::io_context::service(io_svc),
+      work_{asio::make_work_guard(io_svc)} {
     }
 
-    virtual ~service() {}
+    virtual ~rr_service() {}
 
-    service( service const&) = delete;
-    service & operator=( service const&) = delete;
+    rr_service(rr_service const&) = delete;
+    rr_service & operator=(rr_service const&) = delete;
 
     void shutdown_service() final {
-        work_.reset();
+      work_.reset();
     }
   };
 //]
 
 //[asio_rr_ctor
-  round_robin( std::shared_ptr< asio::io_service > const& io_svc) :
-      io_svc_( io_svc),
-      suspend_timer_( * io_svc_) {
-      // We use add_service() very deliberately. This will throw
-      // service_already_exists if you pass the same io_service instance to
-      // more than one round_robin instance.
-      asio::add_service( * io_svc_, new service( * io_svc_) );
+  round_robin(const std::shared_ptr< asio::io_context >& io_svc) :
+      io_svc_(io_svc), suspend_timer_( * io_svc_) {
+    // We use add_service() very deliberately. This will throw
+    // service_already_exists if you pass the same io_context instance to
+    // more than one round_robin instance.
+    asio::add_service(*io_svc_, new rr_service(*io_svc_));
 
-      asio::post(*io_svc_, [this] () mutable { this->loop();});
+    asio::post(*io_svc_, [this] () mutable { this->loop();});
   }
 
   void awakened( fibers::context * ctx) noexcept {
@@ -111,76 +113,76 @@ class round_robin : public fibers::algo::algorithm {
       // VLOG(1) << "awakened: " << ctx->get_id();
   }
 
-    fibers::context * pick_next() noexcept {
-      fibers::context * ctx( nullptr);
-      if ( ! rqueue_.empty() ) { /*<
-          pop an item from the ready queue
-      >*/
-          ctx = & rqueue_.front();
-          rqueue_.pop_front();
-          BOOST_ASSERT( nullptr != ctx);
-          BOOST_ASSERT( fibers::context::active() != ctx);
-          if ( ! ctx->is_context( fibers::type::dispatcher_context) ) {
-              --counter_;
-          }
-      }
-      return ctx;
+  fibers::context * pick_next() noexcept {
+    fibers::context * ctx( nullptr);
+    if ( ! rqueue_.empty() ) { /*<
+        pop an item from the ready queue
+    >*/
+        ctx = & rqueue_.front();
+        rqueue_.pop_front();
+        BOOST_ASSERT( nullptr != ctx);
+        BOOST_ASSERT( fibers::context::active() != ctx);
+        if ( ! ctx->is_context( fibers::type::dispatcher_context) ) {
+            --counter_;
+        }
     }
+    return ctx;
+  }
 
-    bool has_ready_fibers() const noexcept {
-        return 0 < counter_;
-    }
+  bool has_ready_fibers() const noexcept {
+      return 0 < counter_;
+  }
 
-    void suspend_until( std::chrono::steady_clock::time_point const& abs_time) noexcept final {
-        // Set a timer so at least one handler will eventually fire, causing
-        // run_one() to eventually return.
-      if ( (std::chrono::steady_clock::time_point::max)() != abs_time) {
-      // Each expires_at(time_point) call cancels any previous pending
-      // call. We could inadvertently spin like this:
-      // dispatcher calls suspend_until() with earliest wake time
-      // suspend_until() sets suspend_timer_
-      // lambda loop calls run_one()
-      // some other asio handler runs before timer expires
-      // run_one() returns to lambda loop
-      // lambda loop yields to dispatcher
-      // dispatcher finds no ready fibers
-      // dispatcher calls suspend_until() with SAME wake time
-      // suspend_until() sets suspend_timer_ to same time, canceling
-      // previous async_wait()
-      // lambda loop calls run_one()
-      // asio calls suspend_timer_ handler with operation_aborted
-      // run_one() returns to lambda loop... etc. etc.
-      // So only actually set the timer when we're passed a DIFFERENT
-      // abs_time value.
-          suspend_timer_.expires_at( abs_time);
-          suspend_timer_.async_wait([](system::error_code const&){
-                                      this_fiber::yield();
-                                    });
-      }
-      cnd_.notify_one();
+  void suspend_until( std::chrono::steady_clock::time_point const& abs_time) noexcept final {
+      // Set a timer so at least one handler will eventually fire, causing
+      // run_one() to eventually return.
+    if ( (std::chrono::steady_clock::time_point::max)() != abs_time) {
+    // Each expires_at(time_point) call cancels any previous pending
+    // call. We could inadvertently spin like this:
+    // dispatcher calls suspend_until() with earliest wake time
+    // suspend_until() sets suspend_timer_
+    // lambda loop calls run_one()
+    // some other asio handler runs before timer expires
+    // run_one() returns to lambda loop
+    // lambda loop yields to dispatcher
+    // dispatcher finds no ready fibers
+    // dispatcher calls suspend_until() with SAME wake time
+    // suspend_until() sets suspend_timer_ to same time, canceling
+    // previous async_wait()
+    // lambda loop calls run_one()
+    // asio calls suspend_timer_ handler with operation_aborted
+    // run_one() returns to lambda loop... etc. etc.
+    // So only actually set the timer when we're passed a DIFFERENT
+    // abs_time value.
+        suspend_timer_.expires_at( abs_time);
+        suspend_timer_.async_wait([](system::error_code const&){
+                                    this_fiber::yield();
+                                  });
     }
+    cnd_.notify_one();
+  }
 //]
 
   void notify() noexcept {
-      // Something has happened that should wake one or more fibers BEFORE
-      // suspend_timer_ expires. Reset the timer to cause it to fire
-      // immediately, causing the run_one() call to return. In theory we
-      // could use cancel() because we don't care whether suspend_timer_'s
-      // handler is called with operation_aborted or success. However --
-      // cancel() doesn't change the expiration time, and we use
-      // suspend_timer_'s expiration time to decide whether it's already
-      // set. If suspend_until() set some specific wake time, then notify()
-      // canceled it, then suspend_until() was called again with the same
-      // wake time, it would match suspend_timer_'s expiration time and we'd
-      // refrain from setting the timer. So instead of simply calling
-      // cancel(), reset the timer, which cancels the pending sleep AND sets
-      // a new expiration time. This will cause us to spin the loop twice --
-      // once for the operation_aborted handler, once for timer expiration
-      // -- but that shouldn't be a big problem.
-      suspend_timer_.async_wait([](boost::system::error_code const&){
-                                  this_fiber::yield();
-                                });
-      suspend_timer_.expires_at( std::chrono::steady_clock::now() );
+    // Something has happened that should wake one or more fibers BEFORE
+    // suspend_timer_ expires. Reset the timer to cause it to fire
+    // immediately, causing the run_one() call to return. In theory we
+    // could use cancel() because we don't care whether suspend_timer_'s
+    // handler is called with operation_aborted or success. However --
+    // cancel() doesn't change the expiration time, and we use
+    // suspend_timer_'s expiration time to decide whether it's already
+    // set. If suspend_until() set some specific wake time, then notify()
+    // canceled it, then suspend_until() was called again with the same
+    // wake time, it would match suspend_timer_'s expiration time and we'd
+    // refrain from setting the timer. So instead of simply calling
+    // cancel(), reset the timer, which cancels the pending sleep AND sets
+    // a new expiration time. This will cause us to spin the loop twice --
+    // once for the operation_aborted handler, once for timer expiration
+    // -- but that shouldn't be a big problem.
+    suspend_timer_.async_wait([](boost::system::error_code const&){
+                                this_fiber::yield();
+                              });
+    suspend_timer_.expires_at( std::chrono::steady_clock::now() );
   }
 
 //]
@@ -196,7 +198,7 @@ class round_robin : public fibers::algo::algorithm {
             std::unique_lock< fibers::mutex > lk( mtx_);
             cnd_.wait(lk);
         } else {
-            // run one handler inside io_service
+            // run one handler inside io_context
             // if no handler available, block this thread
 
             if ( ! io_svc_->run_one() ) {
@@ -207,24 +209,13 @@ class round_robin : public fibers::algo::algorithm {
     }
 };
 
-asio::io_service::id round_robin::service::id;
+asio::io_context::id round_robin::rr_service::id;
 
 constexpr unsigned max_length = 1024;
 
 /*****************************************************************************
 *   fiber function per server connection
 *****************************************************************************/
-// TODO: there is a bug here:
-// if server stops, this socket still block and the fiber does not exit.
-// There is resource leak and fiber scheduler will deadlock.
-// Solution: we should have the notion of connection that tracks all its
-// resources (fiber, socket etc)
-// Server should access the list of all active connections and release them upon exit.
-// In addition, each fiber should be able to remove its connection from the list when exiting.
-// One way to do it is to use intrusive linked list. That way each connection ptr could remove itself
-// and no allocations are needed. For example, intrusive::slist, like terminated_queue_type in scheduler.hpp.
-// libs/asio/example/cpp03/http/server/server.hpp uses simpler way to handle the connections using
-// regular containers.
 void session(Connection* conn) {
     tcp::socket* sock = &conn->socket;
     try {
@@ -277,12 +268,10 @@ void server(asio::io_service& io_svc, tcp::acceptor & a) {
       for (;;) {
           std::unique_ptr<Connection> conn(new Connection(&io_svc, &empty_list));
           system::error_code ec;
-          a.async_accept(
-                  conn->socket,
-                  fibers::asio::yield[ec]);
+          a.async_accept(conn->socket, fibers::asio::yield[ec]);
 
           if ( ec) {
-              throw system::system_error( ec); //some other error
+            throw system::system_error(ec); //some other error
           } else {
             clist.push_back(*conn);
             DCHECK(!clist.empty());
@@ -314,8 +303,7 @@ void server(asio::io_service& io_svc, tcp::acceptor & a) {
 /*****************************************************************************
 *   fiber function per client
 *****************************************************************************/
-void client(boost::asio::io_service& io_svc,
-            unsigned iterations, unsigned msg_count) {
+void client(boost::asio::io_service& io_svc, unsigned iterations, unsigned msg_count) {
   LOG(INFO) << ": echo-client started";
 
   for (unsigned count = 0; count < iterations; ++count) {
