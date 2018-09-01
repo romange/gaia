47faa6ca7ba171d54198b475b7942891e7e38d60
Roman Gershman <romange@gmail.com>
2018-09-01 10:58:35 +0300

    .

--- strings/strcat_test.cc
+++ strings/strcat_test.cc
@@ -9,9 +9,9 @@ class StrCatTest : public testing::Test {
 
 TEST_F(StrCatTest, Base) {
   char buf[200] = {0};
-  StrAppend(buf, sizeof(buf), {"a", "b", 5, "cdef"});
+  StrAppend(buf, sizeof(buf), "a", "b", 5, "cdef");
   EXPECT_STREQ("ab5cdef", buf);
   memset(buf, 'a', 200);
-  StrAppend(buf, sizeof(buf), {"foo", 5, "bar"});
+  StrAppend(buf, sizeof(buf), "foo", 5, "bar");
   EXPECT_STREQ("foo5bar", buf);
 }
