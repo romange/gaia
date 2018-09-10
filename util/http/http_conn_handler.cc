cb491ed8f4ca00659af3525e30328878c9751725
Roman Gershman <romange@gmail.com>
2018-09-10 20:53:05 +0300

    .

--- util/http/http_conn_handler.cc
+++ util/http/http_conn_handler.cc
@@ -7,7 +7,7 @@
 #include <boost/beast/http.hpp>
 
 #include "absl/strings/str_join.h"
-
+#include "absl/strings/str_split.h"
 #include "base/logging.h"
 #include "strings/stringpiece.h"
 #include "util/asio/yield.h"
@@ -21,20 +21,99 @@ extern string BuildStatusPage();
 namespace util {
 namespace http {
 using namespace boost;
-namespace http2 = beast::http;
+namespace h2 = beast::http;
 
 using fibers_ext::yield;
 
 namespace {
+constexpr char kHtmlMime[] = "text/html";
+
 inline absl::string_view as_absl(::boost::string_view s) {
   return absl::string_view(s.data(), s.size());
 }
 
 inline system::error_code to_asio(system::error_code ec) {
-  if (ec == http2::error::end_of_stream)
+  if (ec == h2::error::end_of_stream)
     return asio::error::eof;
   return ec;
 }
+
+inline std::pair<StringPiece, StringPiece> Parse(::boost::string_view str) {
+  std::pair<StringPiece, StringPiece> res;
+  size_t pos = str.find('?');
+  res.first = as_absl(str.substr(0, pos));
+  if (pos != ::boost::string_view::npos) {
+    res.second = as_absl(str.substr(pos + 1));
+  }
+  return res;
+}
+
+vector<std::pair<StringPiece, StringPiece>> SplitQuery(StringPiece query) {
+  vector<StringPiece> args = absl::StrSplit(query, '&');
+  vector<std::pair<StringPiece, StringPiece>> res(args.size());
+  for (size_t i = 0; i < args.size(); ++i) {
+    size_t pos = args[i].find('=');
+    res[i].first = args[i].substr(0, pos);
+    res[i].second = (pos == StringPiece::npos) ? StringPiece() : args[i].substr(pos + 1);
+  }
+  return res;
+}
+
+void HandleVModule(StringPiece str) {
+  vector<StringPiece> parts = absl::StrSplit(str, ",", absl::SkipEmpty());
+  for (StringPiece p : parts) {
+    size_t sep = p.find('=');
+    int32_t level = 0;
+    if (sep != StringPiece::npos && absl::SimpleAtoi(p.substr(sep + 1), &level)) {
+      string module_expr = strings::AsString(p.substr(0, sep));
+      int prev = google::SetVLOGLevel(module_expr.c_str(), level);
+      LOG(INFO) << "Setting module " << module_expr << " to loglevel " << level
+                << ", prev: " << prev;
+    }
+  }
+}
+
+void ParseFlagz(const QueryArgs& args, h2::response<h2::string_body>* response) {
+  StringPiece flag_name;
+  StringPiece value;
+  for (const auto& k_v : args) {
+    if (k_v.first == "flag") {
+      flag_name = k_v.second;
+    } else if (k_v.first == "value") {
+      value = k_v.second;
+    }
+  }
+  if (!flag_name.empty()) {
+    google::CommandLineFlagInfo flag_info;
+    string fname = strings::AsString(flag_name);
+    if (!google::GetCommandLineFlagInfo(fname.c_str(), &flag_info)) {
+      response->body() = "Flag not found \n";
+    } else {
+      response->set(h2::field::content_type, kHtmlMime);
+      response->body().append("<p>Current value ").append(flag_info.current_value).append("</p>");
+      string new_val = strings::AsString(value);
+      string res = google::SetCommandLineOption(fname.c_str(), new_val.c_str());
+      response->body().append("Flag ").append(res);
+
+      if (flag_name == "vmodule") {
+        HandleVModule(value);
+      }
+    }
+  } else if (args.size() == 1) {
+    LOG(INFO) << "Printing all flags";
+    std::vector<google::CommandLineFlagInfo> flags;
+    google::GetAllFlags(&flags);
+    for (const auto& v : flags) {
+      response->body()
+          .append("--")
+          .append(v.name)
+          .append(": ")
+          .append(v.current_value)
+          .append("\n");
+    }
+  }
+}
+
 }  // namespace
 
 HttpHandler::HttpHandler() {
@@ -43,38 +122,60 @@ HttpHandler::HttpHandler() {
 
 system::error_code HttpHandler::HandleRequest() {
   beast::flat_buffer buffer;
-  http2::request<http2::dynamic_body> request;
+  h2::request<h2::dynamic_body> request;
 
   system::error_code ec;
 
-  http2::async_read(*socket_, buffer, request, yield[ec]);
+  h2::async_read(*socket_, buffer, request, yield[ec]);
   if (ec) {
     return to_asio(ec);
   }
-  http2::response<http2::string_body> response{http2::status::ok, request.version()};
-  response.set(http2::field::server, "GAIA");
+  h2::response<h2::string_body> response{h2::status::ok, request.version()};
+  response.set(h2::field::server, "GAIA");
+
+  VLOG(1) << "Full Url: " << request.target();
 
   if (request.target() == "/favicon.ico") {
-    response.set(http2::field::location, favicon_);
-    response.result(http2::status::moved_permanently);
-  } else if (request.target() == "/") {
-    response.set(http2::field::content_type, "text/html");
-    response.keep_alive(request.keep_alive());
-    response.body() = ::http::BuildStatusPage();
+    response.set(h2::field::location, favicon_);
+    response.result(h2::status::moved_permanently);
   } else {
-    if (VLOG_IS_ON(1)) {
-      LOG(INFO) << "Target: " << request.target();
-      auto formater = [](string* dest, const auto& f) {
-        absl::StrAppend(dest, as_absl(f.name_string()), ":", as_absl(f.value()));
-      };
-      string str = absl::StrJoin(request, ",", formater);
-      VLOG(1) << "Fields: " << str;
+    StringPiece path, query;
+    tie(path, query) = Parse(request.target());
+    auto args = SplitQuery(query);
+
+    if (path == "/") {
+      response.set(h2::field::content_type, kHtmlMime);
+      response.keep_alive(request.keep_alive());
+      response.body() = ::http::BuildStatusPage();
+    } else if (path == "/flagz") {
+      if (Authorize(args)) {
+        ParseFlagz(args, &response);
+      } else {
+        response.result(h2::status::unauthorized);
+      }
+    } else {
+      if (VLOG_IS_ON(1)) {
+        LOG(INFO) << "Target: " << request.target();
+        auto formater = [](string* dest, const auto& f) {
+          absl::StrAppend(dest, as_absl(f.name_string()), ":", as_absl(f.value()));
+        };
+        string str = absl::StrJoin(request, ",", formater);
+        VLOG(1) << "Fields: " << str;
+      }
     }
   }
   response.prepare_payload();
-  http2::async_write(*socket_, response, yield[ec]);
+  h2::async_write(*socket_, response, yield[ec]);
 
   return to_asio(ec);
+}
+
+bool HttpHandler::Authorize(const QueryArgs& args) const {
+  for (const auto& k_v : args) {
+    if (Authorize(k_v.first, k_v.second))
+      return true;
+  }
+  return false;
 }
 
 }  // namespace http
--- util/http/http_conn_handler.h
+++ util/http/http_conn_handler.h
@@ -2,17 +2,29 @@
 // Author: Roman Gershman (romange@gmail.com)
 //
 #include "util/asio/connection_handler.h"
+#include "strings/stringpiece.h"
 
 namespace util {
 namespace http {
+
+// URL consists of path and query delimited by '?'.
+// query can be broken into query args delimited by '&'.
+// Each query arg can be a pair of "key=value" values.
+// In case there is not '=' delimiter, only the first field is filled.
+typedef std::vector<std::pair<StringPiece, StringPiece>> QueryArgs;
 
 class HttpHandler : public ConnectionHandler {
  public:
   HttpHandler();
 
   boost::system::error_code HandleRequest() final override;
-
  private:
+  virtual bool Authorize(StringPiece key, StringPiece value) const {
+    return true;
+  }
+
+  bool Authorize(const QueryArgs& args) const;
+
   const char* favicon_;
 };
 
