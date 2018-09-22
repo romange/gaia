79e66102c081b4aaf3c935a96877a58462ccecc9
Roman Gershman <romange@gmail.com>
2018-09-22 23:19:11 +0300

    Add Buffered socket

--- util/asio/asio_utils.h
+++ util/asio/asio_utils.h
@@ -20,10 +20,42 @@ inline const_buffer buffer(const base::PODArray<uint8_t>& arr) noexcept {
   return const_buffer(arr.data(), arr.size());
 }
 
+inline mutable_buffer* buffer_sequence_begin(mutable_buffer& b) {
+  return &b;
+}
+
+/// Get an iterator to the first element in a buffer sequence.
+inline const_buffer* buffer_sequence_begin(const_buffer& b) {
+  return &b;
+}
+
+inline mutable_buffer* buffer_sequence_end(mutable_buffer& b) {
+  return &b + 1;
+}
+
+/// Get an iterator to the first element in a buffer sequence.
+inline const_buffer* buffer_sequence_end(const_buffer& b) {
+  return &b + 1;
+}
+
 }  // namespace asio
 }  // namespace boost
 
 namespace util {
+
+template<typename It> class ContainerSlice {
+ public:
+  typedef It iterator;
+
+  ContainerSlice() : b_(), e_() {}
+  ContainerSlice(It b, It e) : b_(b), e_(e) {}
+
+  It begin() const noexcept { return b_;}
+  It end() const noexcept { return e_;}
+  size_t size() const { return e_ - b_; }
+ private:
+  It b_, e_;
+};
 
 namespace detail {
 
@@ -31,6 +63,23 @@ namespace detail {
 template <typename... T>
 constexpr auto _MakeCommonBuf(T&&... values) ->
         typename std::common_type<decltype(::boost::asio::buffer(values))...>::type;
+
+template <typename BufferSequence> struct BufferSequenceTraits {
+  using raw_it_t = decltype(::boost::asio::buffer_sequence_begin(*static_cast<BufferSequence*>(0)));
+  using iterator = std::conditional_t<std::is_pointer<raw_it_t>::value,
+                                      std::add_pointer_t<std::remove_const_t<std::remove_pointer_t<raw_it_t>>>,
+                                      raw_it_t>;
+};
+
+
+static_assert(std::is_same<typename BufferSequenceTraits<::boost::asio::mutable_buffer>::iterator,
+                           ::boost::asio::mutable_buffer*>::value, "");
+
+static_assert(std::is_same<typename BufferSequenceTraits<::std::vector<::boost::asio::mutable_buffer>>::iterator,
+                           ::std::vector<::boost::asio::mutable_buffer>::iterator>::value, "");
+
+template<typename BufferSequence> using SliceSeq =
+    ContainerSlice<typename BufferSequenceTraits<BufferSequence>::iterator>;
 
 }  // namespace detail
 
@@ -44,6 +93,52 @@ constexpr auto make_buffer_seq(T&&... values) ->
         std::array<decltype(detail::_MakeCommonBuf(values...)), sizeof...(T)> {
     return {::boost::asio::buffer(values)...};
 }
+
+template <size_t N>
+constexpr std::array<::boost::asio::mutable_buffer, N + 1>
+    make_buffer_seq(const std::array<::boost::asio::mutable_buffer, N>& arr,
+                    const ::boost::asio::mutable_buffer& mbuf) {
+  std::array<::boost::asio::mutable_buffer, N + 1> res;
+  std::copy(arr.begin(), arr.end(), res.begin());
+  res[N] = mbuf;
+  return res;
+}
+
+template <typename It>
+std::vector<typename std::iterator_traits<It>::value_type>
+    make_buffer_seq(const ContainerSlice<It>& slice,
+                    const typename std::iterator_traits<It>::value_type& mbuf) {
+  using value_type = typename std::iterator_traits<It>::value_type;
+  static_assert(!std::is_const<value_type>::value, "");
+
+  std::vector<value_type> res(slice.size() + 1);
+  std::copy(slice.begin(), slice.end(), res.begin());
+  res.back() = mbuf;
+  return res;
+}
+
+template<typename BufferSequence> detail::SliceSeq<BufferSequence>
+StripSequence(size_t size, BufferSequence& arg) {
+  using Slice = detail::SliceSeq<BufferSequence>;
+  using namespace boost::asio;
+
+  auto b = buffer_sequence_begin(arg);
+  auto e = buffer_sequence_end(arg);
+  if (size == 0) {
+    return Slice(b, e);
+  }
+
+  for (; b != e; ++b) {
+    if (b->size() > size) {
+      (*b) += size;  // advance the buffer prefix
+      return Slice(b, e);
+    }
+    size -= b->size();
+  }
+
+  return Slice();
+}
+
 
 #ifdef __clang__
 #pragma clang diagnostic pop
--- util/rpc/frame_format.cc
+++ util/rpc/frame_format.cc
@@ -6,22 +6,18 @@
 
 #include <boost/asio/read.hpp>
 
+#include "absl/strings/escaping.h"
 #include "base/bits.h"
 #include "base/endian.h"
 #include "base/logging.h"
-#include "absl/strings/escaping.h"
 #include "util/asio/yield.h"
 
 namespace util {
 using fibers_ext::yield;
 
 namespace rpc {
-using util::Status;
-using util::StatusCode;
 using namespace boost;
 using namespace system;
-
-namespace gpb = ::google::protobuf;
 
 namespace {
 
@@ -39,27 +35,40 @@ std::ostream& operator<<(std::ostream& o, const Frame& frame) {
   return o;
 }
 
-const uint32 Frame::kHeaderVal =  LittleEndian::Load32(kHeader);
+const uint32 Frame::kHeaderVal = LittleEndian::Load32(kHeader);
 
-
-inline constexpr uint32 byte_mask(uint8 n) { return (1UL << (n + 1) * 8) - 1;}
+inline constexpr uint32 byte_mask(uint8 n) {
+  return (1UL << (n + 1) * 8) - 1;
+}
 
 error_code Frame::Read(socket_t* input) {
   uint8 buf[kMaxByteSize + /* a little extra */ 8];
 
   error_code ec;
-  size_t read = asio::async_read(*input, asio::buffer(buf, kMinByteSize),
-                                 yield[ec]);
-  if (ec) {
+  size_t read;
+  size_t available = 0; /*input->available(ec);
+  if (ec)
     return ec;
+*/
+  if (available >= kMinByteSize) {
+    read = input->read_some(asio::buffer(buf, kMinByteSize), ec);
+    available -= kMinByteSize;
+  } else {
+    read = asio::async_read(*input, asio::buffer(buf, kMinByteSize), yield[ec]);
+    if (ec)
+      return ec;
+    // available = input->available(ec);
   }
+  if (ec)
+    return ec;
 
   DCHECK_EQ(kMinByteSize, read);
-  if (kHeaderVal !=  LittleEndian::Load32(buf)) {
+
+  if (kHeaderVal != LittleEndian::Load32(buf)) {
     return errc::make_error_code(errc::illegal_byte_sequence);
   }
 
-  if (buf[4] >> 4 != 0) { // version check
+  if (buf[4] >> 4 != 0) {  // version check
     return errc::make_error_code(errc::illegal_byte_sequence);
   }
 
@@ -67,26 +76,29 @@ error_code Frame::Read(socket_t* input) {
   rpc_id >>= 8;
 
   const uint8 sz_len = buf[4] & 15;
-  const uint8 control_sz_len_minus1 = sz_len & 3;
+  const uint8 header_sz_len_minus1 = sz_len & 3;
   const uint8 msg_sz_len_minus1 = sz_len >> 2;
+  size_t to_read = header_sz_len_minus1 + msg_sz_len_minus1;
 
   // We stored 2 necessary bytes of boths lens, if it was not enough lets fill em up.
   if (sz_len) {
-    asio::async_read(*input, asio::buffer(buf + kMinByteSize, kMaxByteSize - kMinByteSize),
-                     asio::transfer_exactly(control_sz_len_minus1 + msg_sz_len_minus1),
-                     yield[ec]);
-    if (ec) {
-      return ec;
+    auto mbuf = asio::buffer(buf + kMinByteSize, to_read);
+    if (available >= to_read) {
+      read = input->read_some(mbuf, ec);
+    } else {
+      read = asio::async_read(*input, mbuf, yield[ec]);
     }
+    if (ec)
+      return ec;
   }
 
-  VLOG(2) << "Frame::Read " << kMinByteSize + control_sz_len_minus1 + msg_sz_len_minus1;
+  VLOG(2) << "Frame::Read " << kMinByteSize + to_read;
 
-  header_size = LittleEndian::Load32(buf + 12) & byte_mask(control_sz_len_minus1);
-  letter_size = LittleEndian::Load32(buf + 12 + control_sz_len_minus1 + 1) &
-      byte_mask(msg_sz_len_minus1);
+  header_size = LittleEndian::Load32(buf + 12) & byte_mask(header_sz_len_minus1);
+  letter_size =
+      LittleEndian::Load32(buf + 12 + header_sz_len_minus1 + 1) & byte_mask(msg_sz_len_minus1);
 
-  return error_code{};
+  return ec;
 }
 
 unsigned Frame::Write(uint8* dest) const {
@@ -100,7 +112,8 @@ unsigned Frame::Write(uint8* dest) const {
 
   uint64_t version = cntrl_bytes_minus1 | (msg_bytes_minus1 << 2) | (0 << 4);
 
-  LittleEndian::Store64(dest, (rpc_id << 8) | version); dest += 8;
+  LittleEndian::Store64(dest, (rpc_id << 8) | version);
+  dest += 8;
 
   LittleEndian::Store32(dest, header_size);
   dest += (cntrl_bytes_minus1 + 1);
@@ -109,34 +122,46 @@ unsigned Frame::Write(uint8* dest) const {
   return 4 + 1 /* version */ + 7 /* rpc_id */ + cntrl_bytes_minus1 + msg_bytes_minus1 + 2;
 }
 
-#if 0
-Status ReadPacket(Frame::socket_t* input,
-                  Frame* frame, std::string* control, std::string* msg) {
-  Status st = frame->Read(input);
-  if (!st.ok()) {
-    VLOG(1) << "Bad frame header " << st;
-    return st; // If we get EOF here it is not an error, that's why we just return st.
+::boost::system::error_code Frame::Read(BufferedSocketReadAdaptor<socket_t>* input) {
+  uint8 buf[kMaxByteSize + /* a little extra */ 8];
+
+  error_code ec = input->Read(asio::mutable_buffer(buf, kMinByteSize));
+  if (ec)
+    return ec;
+
+
+  if (kHeaderVal != LittleEndian::Load32(buf)) {
+    return errc::make_error_code(errc::illegal_byte_sequence);
   }
 
-  VLOG(1) << "ReadPacket. Frame: " << *frame;
-  control->resize(frame->header_size);
-  msg->resize(frame->letter_size);
-
-  if (!(st = input->readAll((uint8*)&control->front(), frame->header_size)).ok() ||
-      !(st = input->readAll((uint8*)&msg->front(), frame->letter_size)).ok()) {
-    if (st.code() == StatusCode::IO_END_OF_FILE)
-      return StatusCode::IO_ERROR; // EOF in the middle of the message is an error
-    return st;
+  if (buf[4] >> 4 != 0) {  // version check
+    return errc::make_error_code(errc::illegal_byte_sequence);
   }
 
-  VLOG(2) << "Control read: " <<  absl
-  ::CEscape(*control);
-  VLOG(2) << "msg read: " <<  absl::CEscape(*msg);
+  rpc_id = UNALIGNED_LOAD64(buf + 4);
+  rpc_id >>= 8;
 
+  const uint8 sz_len = buf[4] & 15;
+  const uint8 header_sz_len_minus1 = sz_len & 3;
+  const uint8 msg_sz_len_minus1 = sz_len >> 2;
+  size_t to_read = header_sz_len_minus1 + msg_sz_len_minus1;
 
-  return Status::OK;
+  // We stored 2 necessary bytes of boths lens, if it was not enough lets fill em up.
+  if (sz_len) {
+    auto mbuf = asio::mutable_buffer(buf + kMinByteSize, to_read);
+    ec = input->Read(mbuf);
+    if (ec)
+      return ec;
+  }
+
+  VLOG(2) << "Frame::Read " << kMinByteSize + to_read;
+
+  header_size = LittleEndian::Load32(buf + 12) & byte_mask(header_sz_len_minus1);
+  letter_size =
+      LittleEndian::Load32(buf + 12 + header_sz_len_minus1 + 1) & byte_mask(msg_sz_len_minus1);
+
+  return ec;
 }
-#endif
 
 }  // namespace rpc
 
