b544c0f4d374efab94baa782840dc1bd696ae7be
Roman Gershman <romange@gmail.com>
2018-09-22 08:48:56 +0300

    .

---
+++ util/asio/io_context_test.cc
@@ -0,0 +1,43 @@
+// Copyright 2018, Beeri 15.  All rights reserved.
+// Author: Roman Gershman (romange@gmail.com)
+//
+#include <boost/asio.hpp>
+
+#include "base/gtest.h"
+#include "base/logging.h"
+#include "base/walltime.h"
+
+using namespace std::chrono;
+using namespace boost;
+using namespace asio;
+
+namespace util {
+
+class IoContextTest : public testing::Test {
+ protected:
+  void SetUp() override {
+  }
+
+  void TearDown() {
+  }
+};
+
+TEST_F(IoContextTest, Basic) {
+  io_context cntx(1);   // no locking
+  int i = 0;
+  cntx.post([&i] {++i;});
+  EXPECT_EQ(0, i);
+  EXPECT_EQ(1, cntx.run_one());
+  EXPECT_EQ(1, i);
+  EXPECT_EQ(0, cntx.run_one());
+}
+
+static void BM_RunOne(benchmark::State& state) {
+  io_context cntx(state.range_x());   // no locking
+  while (state.KeepRunning()) {
+    cntx.run_one();
+  }
+}
+BENCHMARK(BM_RunOne)->Arg(0)->Arg(1);
+
+}  // namespace util
