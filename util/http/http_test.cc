6feae1427ee83ba3705b6c7331b81db3f98c9d62
Roman Gershman <romange@gmail.com>
2018-11-30 12:35:19 +0200

    Add http client test

--- util/http/http_test.cc
+++ util/http/http_test.cc
@@ -1,6 +1,11 @@
 // Copyright 2018, Beeri 15.  All rights reserved.
 // Author: Roman Gershman (romange@gmail.com)
 //
+#include <boost/asio/connect.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+#include <boost/beast/http/dynamic_body.hpp>
+#include <boost/beast/http/read.hpp>
+
 #include "base/gtest.h"
 #include "base/logging.h"
 
@@ -9,6 +14,8 @@
 #include "util/http/http_conn_handler.h"
 
 namespace util {
+
+using namespace boost;
 
 class HttpTest : public testing::Test {
  protected:
@@ -39,8 +46,47 @@ void HttpTest::TearDown() {
   pool_->Stop();
 }
 
-TEST_F(HttpTest, Basic) {
+using namespace asio::ip;
+namespace h2 = beast::http;
+
+TEST_F(HttpTest, Client) {
+  IoContext& io_context = pool_->GetNextContext();
+
+  tcp::resolver resolver{io_context.get_context()};
+  tcp::socket socket{io_context.get_context()};
+  tcp::endpoint ep(tcp::v4(), port_);
+
+  auto results = resolver.resolve(ep);
+  system::error_code ec;
+
+  // Make the connection on the IP address we get from a lookup
+  asio::connect(socket, results.begin(), results.end(), ec);
+  ASSERT_FALSE(ec);
+
+  // Set the URL
+  h2::request<h2::string_body> req{h2::verb::get, "/foo", 11};
+
+  // Optional headers
+  req.set(h2::field::user_agent, "mytest");
+
+  // Send the HTTP request to the remote host.
+  size_t sz = h2::write(socket, req, ec);
+  ASSERT_FALSE(ec);
+  EXPECT_GT(sz, 0);
+
+  // This buffer is used for reading and must be persisted
+  beast::flat_buffer buffer;
+
+  // Declare a container to hold the response
+  h2::response<h2::dynamic_body> res;
+
+  // Receive the HTTP response
+  sz = h2::read(socket, buffer, res, ec);
+  ASSERT_FALSE(ec);
+  EXPECT_GT(sz, 0);
+
+  // Write the message to standard out
+  VLOG(1) << res;
 }
 
 }  // namespace util
-
