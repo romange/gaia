f67f7fe6a9f9804bf4585df7d6352782f3e8d60f
Roman Gershman <romange@gmail.com>
2018-12-03 07:05:11 +0200

    Refactor http test

--- util/http/http_test.cc
+++ util/http/http_test.cc
@@ -6,7 +6,7 @@
 
 #include "util/asio/accept_server.h"
 #include "util/asio/io_context_pool.h"
-#include "util/http/http_conn_handler.h"
+#include "util/http/http_testing.h"
 #include "util/http/http_client.h"
 
 namespace util {
@@ -14,34 +14,9 @@ namespace util {
 using namespace boost;
 using namespace std;
 
-class HttpTest : public testing::Test {
+class HttpTest : public HttpBaseTest {
  protected:
-  void SetUp() final;
-
-  void TearDown() final;
-
-  std::unique_ptr<AcceptServer> server_;
-  std::unique_ptr<IoContextPool> pool_;
-  http::Listener<> listener_;
-  uint16_t port_ = 0;
 };
-
-void HttpTest::SetUp() {
-  pool_.reset(new IoContextPool);
-  pool_->Run();
-
-  server_.reset(new AcceptServer(pool_.get()));
-  port_ = server_->AddListener(0, &listener_);
-  server_->Run();
-}
-
-void HttpTest::TearDown() {
-  LOG(INFO) << "HttpTest::TearDown";
-
-  server_.reset();
-  VLOG(1) << "After server reset";
-  pool_->Stop();
-}
 
 using namespace asio::ip;
 
