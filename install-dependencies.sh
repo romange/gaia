d2872f51169a655243bde6cbcda8946b506209f7
Roman Gershman <romange@gmail.com>
2018-12-16 08:38:59 +0200

    .

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -18,7 +18,7 @@ install_boost() {
 
     cd $BOOST && ./bootstrap.sh --prefix=/opt/boost --without-libraries=graph_parallel,graph,wave,test,mpi,python
     ./b2 --link=shared cxxflags="-std=c++14 -Wno-deprecated-declarations"  --variant=release --threading=multi \
-         --without-test --without-python -j4
+         --without-test --without-math --without-locale --without-wave --without-regex --without-python -j4
     ./b2 install -d0
 }
 
