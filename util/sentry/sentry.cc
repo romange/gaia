cbf6d2ad48c278b6e3b5da51c589eaf31e5107cb
Roman Gershman <romange@gmail.com>
2018-12-26 22:37:13 +0200

    .

--- util/sentry/sentry.cc
+++ util/sentry/sentry.cc
@@ -9,32 +9,89 @@
 #include "util/asio/glog_asio_sink.h"
 #include "util/http/http_client.h"
 
+DEFINE_string(sentry_dsn, "", "Sentry DSN in the format "
+                              "<pivatekey>@hostname/<project_id>");
+
 namespace util {
 using namespace ::boost;
 using namespace ::std;
 
 namespace {
 
+struct Dsn {
+  string key;
+  string hostname;
+  string project_id;
+};
+
+
 class SentrySink : public GlogAsioSink {
  public:
-  explicit SentrySink(IoContext* io_context) : client_(io_context) {
-  }
+  explicit SentrySink(Dsn dsn, IoContext* io_context);
 
  protected:
   void HandleItem(const Item &item) final;
 
+  bool ShouldIgnore(google::LogSeverity severity, const char *full_filename, int line) final {
+    return severity < google::GLOG_ERROR;
+  }
+
  private:
   http::Client client_;
+  Dsn dsn_;
+  string port_;
 };
 
-void SentrySink::HandleItem(const Item &item) {
+SentrySink::SentrySink(Dsn dsn, IoContext* io_context) : client_(io_context), dsn_(std::move(dsn)) {
+  size_t pos = dsn_.hostname.find(':');
+  if (pos != string::npos) {
+    port_ = dsn_.hostname.substr(pos + 1);
+    dsn_.hostname.resize(pos);
+  } else {
+    port_ = "80";
+  }
+}
 
+void SentrySink::HandleItem(const Item& item) {
+  auto ec = client_.Connect(dsn_.hostname, port_);
+  if (ec) {
+    ++lost_messages_;
+    return;
+  }
+
+  http::Client::Response resp;
+  ec = client_.Get(dsn_.project_id, &resp);
+  if (ec) {
+    ++lost_messages_;
+  } else {
+    LOG(INFO) << "Success";
+  }
+}
+
+bool ParseDsn(const string& dsn, Dsn* res) {
+  CHECK(!dsn.empty());
+  size_t kpos = dsn.find('@');
+  if (kpos == string::npos)
+    return false;
+  res->key = dsn.substr(0, kpos);
+  ++kpos;
+  size_t pos = dsn.find('/', kpos);
+  if (kpos == string::npos)
+    return false;
+  res->hostname = dsn.substr(kpos, pos - kpos);
+  res->project_id = dsn.substr(pos);
+
+  VLOG(1) << "Dsn is: " << res->key << "|" << res->hostname << "|" << res->project_id;
+  return true;
 }
 
 }  // namespace
 
 void EnableSentry(IoContext* context) {
-  auto ptr = std::make_unique<SentrySink>(context);
+  Dsn dsn;
+  CHECK(ParseDsn(FLAGS_sentry_dsn, &dsn)) << "Could not parse " << FLAGS_sentry_dsn;
+
+  auto ptr = std::make_unique<SentrySink>(std::move(dsn), context);
   context->AttachCancellable(ptr.release());
 }
 
--- util/sentry/sentry_test.cc
+++ util/sentry/sentry_test.cc
@@ -4,12 +4,17 @@
 #include "util/sentry/sentry.h"
 
 #include "base/logging.h"
+#include "strings/strcat.h"
 #include "util/http/http_testing.h"
+
+DECLARE_string(sentry_dsn);
 
 namespace util {
 
 using namespace boost;
 using namespace std;
+using namespace chrono_literals;
+namespace h2 = beast::http;
 
 namespace {
 
@@ -17,10 +22,25 @@ namespace {
 
 class SentryTest : public util::HttpBaseTest {
  protected:
+  unsigned req_ = 0;
 };
 
-using namespace asio::ip;
 
+TEST_F(SentryTest, Basic) {
+  listener_.RegisterCb("/id", false,
+      [this](const http::QueryArgs& args, http::HttpHandler::SendFunction* send) {
+    this->req_++;
+    http::StringResponse resp = http::MakeStringResponse(h2::status::ok);
+
+    return send->Invoke(std::move(resp));
+  });
+
+  FLAGS_sentry_dsn = absl::StrCat("foo@localhost:", port_, "/id");
+  EnableSentry(&pool_->GetNextContext());
+  LOG(ERROR) << "Try";
+  this_fiber::sleep_for(10ms);
+  EXPECT_EQ(1, req_);
+}
 
 
 }  // namespace util
