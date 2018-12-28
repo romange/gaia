505879cab64b707965a52805dfef0ea58e8e9aa7
Roman Gershman <romange@gmail.com>
2018-12-28 15:46:54 +0200

    .

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,34 +2,31 @@
 
 set -e
 
-apt install -y cmake libevent-dev libunwind-dev zip flex bison ninja-build autoconf-archive
+apt install -y cmake libunwind-dev zip flex bison ninja-build autoconf-archive
+apt install -y curl
 
-# for folly & proxygen. gperf is not related to gperftools.
-apt install -y gperf curl
-
-BOOST_VER=boost_1_68_0
+BVER=1.69.0
+BOOST=boost_${BVER//./_}   # replace all . with _
 
 install_boost() {
-    BOOST=$BOOST_VER
-    if ! [ -d $BOOST_VER ]; then
-      wget -nv http://dl.bintray.com/boostorg/release/1.68.0/source/$BOOST.tar.bz2 \
+    if ! [ -d $BOOST ]; then
+      wget -nv http://dl.bintray.com/boostorg/release/${BVER}/source/$BOOST.tar.bz2 \
         && tar -xjf $BOOST.tar.bz2
     fi
 
-    cd $BOOST && ./bootstrap.sh --prefix=/opt/boost --without-libraries=graph_parallel,graph,wave,test,mpi,python
-    b2_args=(link=shared variant=release debug-symbols=on threading=multi
-             --without-test --without-math --without-log --without-locale --without-wave
+    cd $BOOST && ./bootstrap.sh --prefix=/opt/${BOOST} --without-libraries=graph_parallel,graph,wave,test,mpi,python
+    b2_args=(define=BOOST_COROUTINES_NO_DEPRECATION_WARNING=1 link=shared variant=release debug-symbols=off
+             threading=multi --without-test --without-math --without-log --without-locale --without-wave
              --without-regex --without-python -j4)
 
-    ./b2 "${b2_args[@]}" cxxflags="-std=c++14 -Wno-deprecated-declarations -fPIC -O3"  \
-
+    ./b2 "${b2_args[@]}" cxxflags="-std=c++14 -Wno-deprecated-declarations -fPIC -O3"
     ./b2 install "${b2_args[@]}" -d0
 }
 
-if ! [ -d /opt/boost/lib ]; then
+if ! [ -d /opt/${BOOST}/lib ]; then
   install_boost
+  ln -sf /opt/${BOOST} /opt/boost
 else
-  echo "Skipping installing boost"
+  echo "Skipping installing ${BOOST}"
 fi
-
 
