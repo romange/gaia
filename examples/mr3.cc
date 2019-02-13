8673070c9b111ffccb876f5e6efbb4f0188fdcd7
Roman Gershman <romange@gmail.com>
2019-02-13 12:06:49 +0200

    .

--- examples/mr3.cc
+++ examples/mr3.cc
@@ -7,6 +7,7 @@
 
 #include "mr/mr3.pb.h"
 #include "util/asio/io_context_pool.h"
+#include "util/status.h"
 
 using namespace std;
 
@@ -18,6 +19,22 @@ class OutputBase {
  public:
 };
 
+class InputBase {
+ public:
+  InputBase(const InputBase&) = delete;
+
+  InputBase(const string& name, pb::WireFormat::Type type) {
+    input_.set_name(name);
+    input_.mutable_format()->set_type(type);
+  }
+
+  pb::Input* data() { return &input_; }
+
+  void operator=(const InputBase&) = delete;
+ protected:
+  pb::Input input_;
+};
+
 template <typename T> class Output : public OutputBase {
  public:
   Output& WithSharding(std::function<string(const T&)> cb);
@@ -26,8 +43,13 @@ template <typename T> class Output : public OutputBase {
 
 template <typename T> class Stream {
   Output<T> out_;
+  pb::Operator op_;
 
  public:
+  Stream(const string& input_name) {
+    op_.add_input_name(input_name);
+  }
+
   Output<T>& Write(const string& name) { return out_; }
 };
 
@@ -37,22 +59,39 @@ class Pipeline {
  public:
   StringStream& ReadText(const string& name, const string& glob);
 
+
+  util::Status Run();
  private:
+  std::vector<std::unique_ptr<InputBase>> inputs_;
+
   std::vector<std::unique_ptr<StringStream>> streams_;
 };
 
 StringStream& Pipeline::ReadText(const string& name, const string& glob) {
-  streams_.emplace_back(new StringStream());
-  return *streams_.back();
+  std::unique_ptr<InputBase> ib(new InputBase(name, pb::WireFormat::TXT));
+  ib->data()->add_file_spec()->set_url_glob(glob);
+  inputs_.emplace_back(std::move(ib));
+
+  streams_.emplace_back(new StringStream(name));
+  auto& ptr = streams_.back();
+
+  return *ptr;
 }
 
 template <typename T> Output<T>& Output<T>::WithSharding(std::function<string(const T&)> cb) {
   return *this;
 }
 
+
 template <typename T>
 Output<T>& Output<T>::AndCompress(pb::Output::CompressType ct, unsigned level) {
   return *this;
+}
+
+using util::Status;
+
+Status Pipeline::Run() {
+  return Status::OK;
 }
 
 }  // namespace mr3
@@ -62,9 +101,9 @@ int main(int argc, char** argv) {
   MainInitGuard guard(&argc, &argv);
 
   Pipeline p;
-  
-  p.ReadText("inp1", "*.csv.gz")
-      .Write("outp1")
+
+  StringStream& ss = p.ReadText("inp1", "*.csv.gz");
+  ss.Write("outp1")
       .AndCompress(pb::Output::GZIP)
       .WithSharding([](const string& str) { return "shardname"; });
 
