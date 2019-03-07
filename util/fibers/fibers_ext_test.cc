68d36dfb209c9f8cd55fdfb62acf34f58398156c
Roman Gershman <romange@gmail.com>
2019-03-07 17:41:05 +0200

    .

--- util/fibers/fibers_ext_test.cc
+++ util/fibers/fibers_ext_test.cc
@@ -3,6 +3,9 @@
 //
 #include "util/fibers/simple_channel.h"
 #include "base/gtest.h"
+#include "base/walltime.h"
+
+using namespace boost;
 
 namespace util {
 namespace fibers_ext {
@@ -26,6 +29,43 @@ TEST_F(FibersTest, SimpleChannel) {
   EXPECT_EQ(2, val);
   ASSERT_TRUE(channel.Pop(val));
   EXPECT_EQ(4, val);
+}
+
+TEST_F(FibersTest, EventCount) {
+  EventCount ec;
+  bool signal = false;
+  bool fb_exit = false;
+
+  fibers::fiber fb(fibers::launch::dispatch, [&] {
+    ec.await([&] { return signal;});
+    fb_exit = true;
+  });
+  ec.notify();
+  this_fiber::yield();
+  EXPECT_FALSE(fb_exit);
+
+  signal = true;
+  ec.notify();
+  fb.join();
+}
+
+TEST_F(FibersTest, SpuriousNotify) {
+  EventCount ec;
+
+  ASSERT_FALSE(ec.notify());
+  ASSERT_FALSE(ec.notifyAll());
+
+  int val = 0;
+
+  auto check_positive = [&val]() -> bool { return val > 0; };
+  std::thread t1([check_positive, &ec]() { ec.await(check_positive);});
+
+  SleepForMilliseconds(1);
+  val = 1;
+  ASSERT_TRUE(ec.notify());
+  t1.join();
+  
+  ASSERT_FALSE(ec.notify());
 }
 
 }  // namespace fibers_ext
