7d7e57a0d8223f1c4151d7798b8a4df8c7dd4c52
Roman Gershman <romange@gmail.com>
2019-03-08 15:03:57 +0200

    .

--- util/fibers/fibers_ext_test.cc
+++ util/fibers/fibers_ext_test.cc
@@ -1,9 +1,10 @@
 // Copyright 2019, Beeri 15.  All rights reserved.
 // Author: Roman Gershman (romange@gmail.com)
 //
-#include "util/fibers/simple_channel.h"
 #include "base/gtest.h"
 #include "base/walltime.h"
+#include "util/fibers/fiberqueue_threadpool.h"
+#include "util/fibers/simple_channel.h"
 
 using namespace boost;
 
@@ -12,11 +13,9 @@ namespace fibers_ext {
 
 class FibersTest : public testing::Test {
  protected:
-  void SetUp() final {
-  }
+  void SetUp() final {}
 
-  void TearDown() final {
-  }
+  void TearDown() final {}
 };
 
 TEST_F(FibersTest, SimpleChannel) {
@@ -30,16 +29,12 @@ TEST_F(FibersTest, SimpleChannel) {
   ASSERT_TRUE(channel.Pop(val));
   EXPECT_EQ(4, val);
 
-  fibers::fiber fb(fibers::launch::post, [&] {
-    EXPECT_TRUE(channel.Pop(val));
-  });
+  fibers::fiber fb(fibers::launch::post, [&] { EXPECT_TRUE(channel.Pop(val)); });
   channel.Push(7);
   fb.join();
   EXPECT_EQ(7, val);
 
-  fb = fibers::fiber(fibers::launch::post, [&] {
-    EXPECT_FALSE(channel.Pop(val));
-  });
+  fb = fibers::fiber(fibers::launch::post, [&] { EXPECT_FALSE(channel.Pop(val)); });
   channel.StartClosing();
   fb.join();
 }
@@ -50,7 +45,7 @@ TEST_F(FibersTest, EventCount) {
   bool fb_exit = false;
 
   fibers::fiber fb(fibers::launch::dispatch, [&] {
-    ec.await([&] { return signal;});
+    ec.await([&] { return signal; });
     fb_exit = true;
   });
   ec.notify();
@@ -71,7 +66,7 @@ TEST_F(FibersTest, SpuriousNotify) {
   int val = 0;
 
   auto check_positive = [&val]() -> bool { return val > 0; };
-  std::thread t1([check_positive, &ec]() { ec.await(check_positive);});
+  std::thread t1([check_positive, &ec]() { ec.await(check_positive); });
 
   SleepForMilliseconds(1);
   val = 1;
@@ -79,6 +74,17 @@ TEST_F(FibersTest, SpuriousNotify) {
   t1.join();
 
   ASSERT_FALSE(ec.notify());
+}
+
+TEST_F(FibersTest, FQTP) {
+  FiberQueueThreadPool pool(1, 2);
+
+  for (unsigned i = 0; i < 10000; ++i) {
+    ASSERT_EQ(i, pool.Await([=] {
+      sched_yield();
+      return i;
+    }));
+  }
 }
 
 }  // namespace fibers_ext
