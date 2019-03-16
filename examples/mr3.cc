4f12664e6c619a971a09b1af6804241b8a0740a0
Roman Gershman <romange@gmail.com>
2019-03-16 23:21:06 +0200

    .

--- examples/mr3.cc
+++ examples/mr3.cc
@@ -62,22 +62,41 @@ int main(int argc, char** argv) {
 1. How do we allow flush hooks ?
 2. How do we allow joining of several collections of different types into a single
 joining object?
+3. PTable<string> is specialization that has additional methods to parse into different types like
+AsJson(), As<PbType> etc.
+4. Pipeline::ReadXXX() methods return PTable that with only input fields set.
+5. By applying mapper we create another table. Sharding also creates another table.
+  Mapper + Sharding can be applied together. To summarize:
+  tables are string collections optionally parseable to custom types.
+  Operations on tables create another tables. Some operations can be set together.
+  Table<X>.Shard(...) -> another Table<X>. Shards are typed so they must follow mapper.
+  Mapper are the ones that change the type of the table. Besides parsing of Table<string>.
+  So it's : Table<X> -> MapTo<Y> returns another Table<Y> by value, ->
+    (optional Shard returns Table<Y> by value which points to the same Table<Y> but with filled sharding infp)
+     -> Write(...) returns *this since it changes the same object.
+  or: Table<X> -> Shard returns Table<X> by value (new tables but sharded with identity mapping),
+  Write returns *this.
 
-mr3::Input<rapidjson::Document> inp1 = p.ReadText("inp1", input1.json.gz").AsJson();
-     mr3::Input<rapidjson::Document> inp2 = p.ReadText("inp2", "input2.json.gz").AsJson();
+struct MapperClass {
+  void operator(rapidjson::Document&& doc, mr3::DoContext<pb::pb::AddresBook>* ctx);
 
-     mr3::PCollection<pb::AddressBook> col1 = inp1.Apply("MyApplyFunc",
-        [] (rapidjson::Document&& doc, mr3::DoContext<pb::AddressBook>* ctx) {
-          pb::AddressBook res;
-          ctx->Write(std::move(res));
-        });
+};
+
+mr3::PTable<rapidjson::Document> inp1 = p.ReadText("inp1", input1.json.gz").AsJson();
+     mr3::PTable<rapidjson::Document> inp2 = p.ReadText("inp2", "input2.json.gz").AsJson();
+
+     mr3::PTable<pb::AddressBook> col1 = p.Map<MapperClass>("MyApplyFunc", inp1);
+
+  // inp1 != inp3 since inp3 is sharded
+  mr3::PTable<pb::AddressBook> inp3 = inp1.Write(...).WithSharding(...);
+
 
   col1.ShardByKey([](const pb::AddressBook& ab) { return ab.user_id();});
   col1.Write(...);
 
-  mr3::PCollection<pb::UserInfo> col2 = inp2.Apply("ReadUsers",
+  mr3::PTable<pb::UserInfo> col2 = p.Map<Map2Class>("ReadUsers",
         [] (rapidjson::Document&& doc, mr3::DoContext<pb::UserInfo>* ctx) {
-          pb::AddressBook res;
+          pb::UserInfo res;
           ctx->Write(std::move(res));
         });
 
