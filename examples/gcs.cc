b57e2ab78a561e251d890e1032495da1d03f1aa6
Roman Gershman <romange@gmail.com>
2019-04-26 21:40:02 +0300

    Parse and fetch tokens

--- examples/gcs.cc
+++ examples/gcs.cc
@@ -7,12 +7,17 @@
 #include <boost/beast/http/read.hpp>
 #include <boost/beast/http/string_body.hpp>
 #include <boost/beast/http/write.hpp>  // for operator<<
-#include <boost/beast/websocket/ssl.hpp>
+
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
 
 #include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/strip.h"
 #include "base/init.h"
 #include "base/logging.h"
 #include "file/file_util.h"
+#include "file/filesource.h"
 #include "util/asio/accept_server.h"
 #include "util/asio/io_context_pool.h"
 #include "util/http/http_client.h"
@@ -21,17 +26,106 @@ using namespace std;
 using namespace boost;
 using namespace util;
 namespace h2 = beast::http;
+namespace rj = rapidjson;
 using asio::ip::tcp;
-
-DEFINE_string(gs_oauth2_refresh_token, "", "");
-DEFINE_string(client_secret, "", "");
-DEFINE_string(client_id, "", "");
 
 const char kDomain[] = "oauth2.googleapis.com";
 const char kService[] = "443";
 
+class GCE {
+ public:
+  GCE() = default;
 
-void Run(IoContext& io_context) {
+  util::Status Init();
+
+  const std::string& project_id() const { return project_id_; }
+  const std::string& client_id() const { return client_id_; }
+  const std::string& client_secret() const { return client_secret_; }
+  const std::string& account_id() const { return account_id_; }
+  const std::string& refresh_token() const { return refresh_token_; }
+
+ private:
+  util::Status ParseDefaultConfig();
+
+  std::string project_id_, client_id_, client_secret_, account_id_, refresh_token_;
+  unique_ptr<asio::ssl::context> ssl_ctx_;
+};
+
+util::Status GCE::ParseDefaultConfig() {
+  string config = file_util::ExpandPath("~/.config/gcloud/configurations/config_default");
+  if (!file::Exists(config)) {
+    return Status("Could not find config_default");
+  }
+  file::LineReader reader(config);
+  string scratch;
+  StringPiece line;
+
+  while (reader.Next(&line, &scratch)) {
+    vector<StringPiece> vals = absl::StrSplit(line, "=");
+    if (vals.size() != 2)
+      continue;
+    for (auto& v : vals) {
+      v = absl::StripAsciiWhitespace(v);
+    }
+    if (vals[0] == "account") {
+      account_id_ = string(vals[1]);
+    } else if (vals[0] == "project") {
+      project_id_ = string(vals[1]);
+    }
+  }
+
+  if (account_id_.empty() || project_id_.empty()) {
+    return Status("Could not find required fields in config_default");
+  }
+  return Status::OK;
+}
+
+util::Status GCE::Init() {
+  string cert;
+  if (!file_util::ReadFileToString("/etc/ssl/certs/ca-certificates.crt", &cert)) {
+    return Status("Could not find certificates");
+  }
+  ssl_ctx_.reset(new asio::ssl::context{asio::ssl::context::tlsv12_client});
+  ssl_ctx_->set_verify_mode(asio::ssl::verify_peer);
+  boost::system::error_code ec;
+  ssl_ctx_->add_certificate_authority(asio::buffer(cert), ec);
+  if (ec) {
+    return Status(ec.message());
+  }
+  RETURN_IF_ERROR(ParseDefaultConfig());
+  LOG(INFO) << "Found account " << account_id_ << "/" << project_id_;
+
+  string adc_file = file_util::ExpandPath(
+      absl::StrCat("~/.config/gcloud/legacy_credentials/", account_id_, "/adc.json"));
+
+  string adc;
+  if (!file_util::ReadFileToString(adc_file, &adc)) {
+    return Status("Could not find " + adc_file);
+  }
+
+  rj::Document adc_doc;
+  constexpr unsigned kFlags = rj::kParseTrailingCommasFlag | rj::kParseCommentsFlag;
+  adc_doc.ParseInsitu<kFlags>(&adc.front());
+
+  if (adc_doc.HasParseError()) {
+    return Status(rj::GetParseError_En(adc_doc.GetParseError()));
+  }
+  for (auto it = adc_doc.MemberBegin(); it != adc_doc.MemberEnd(); ++it) {
+    if (it->name == "client_id") {
+      client_id_ = it->value.GetString();
+    } else if (it->name == "client_secret") {
+      client_secret_ = it->value.GetString();
+    } else if (it->name == "refresh_token") {
+      refresh_token_ = it->value.GetString();
+    }
+  }
+  if (client_id_.empty() || client_secret_.empty() || refresh_token_.empty()) {
+    return Status("Did not find secret tokens");
+  }
+  return Status::OK;
+}
+
+void Run(IoContext& io_context, GCE& gce) {
   string cert;
   file_util::ReadFileToStringOrDie("/etc/ssl/certs/ca-certificates.crt", &cert);
   asio::ssl::context ctx(asio::ssl::context::tlsv12_client);
@@ -61,9 +155,9 @@ void Run(IoContext& io_context) {
 
   string body;
 
-  absl::StrAppend(&body, "client_secret=", FLAGS_client_secret, "&grant_type=refresh_token",
-                  "&refresh_token=", FLAGS_gs_oauth2_refresh_token);
-  absl::StrAppend(&body, "&client_id=", FLAGS_client_id);
+  absl::StrAppend(&body, "client_secret=", gce.client_secret(), "&grant_type=refresh_token",
+                  "&refresh_token=", gce.refresh_token());
+  absl::StrAppend(&body, "&client_id=", gce.client_id());
 
   /*http::Client client(&io_context);
 
@@ -99,14 +193,13 @@ void Run(IoContext& io_context) {
 int main(int argc, char** argv) {
   MainInitGuard guard(&argc, &argv);
 
-  CHECK(!FLAGS_client_id.empty() && !FLAGS_client_secret.empty() &&
-        !FLAGS_gs_oauth2_refresh_token.empty());
-
+  GCE gce;
+  CHECK_STATUS(gce.Init());
   IoContextPool pool;
   pool.Run();
 
   IoContext& io_context = pool.GetNextContext();
-  io_context.AwaitSafe([&] { Run(io_context); });
+  io_context.AwaitSafe([&] { Run(io_context, gce); });
 
   return 0;
 }
