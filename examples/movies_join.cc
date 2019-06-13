db0d8380b2be82522adeb459808c927687fe050d
Roman Gershman <romange@gmail.com>
2019-06-13 23:05:50 +0300

    .

--- examples/movies_join.cc
+++ examples/movies_join.cc
@@ -5,6 +5,7 @@
 // https://www.kaggle.com/rounakbanik/the-movies-dataset/
 
 #include <rapidjson/error/en.h>
+#include <regex>
 
 #include "base/init.h"
 #include "base/logging.h"
@@ -24,29 +25,68 @@ namespace rj = rapidjson;
 DEFINE_string(dest_dir, "~/mr_output", "");
 DEFINE_string(movie_dir, "", "The movies database directory that contains all the files");
 
-/*struct CastCrewObj {
-  rj::Document cast, crew;
-  uint32_t movie_id;
-};
+static void CleanBadJson(char* b, char* e) {
+  // Clean single quotes for keys and optionally for value strings.
+  regex re("(')[^']+('): (?:(').*?(')(?:,|}))?");
+  cmatch m;
 
-namespace mr3 {
+  char* next = b;
+  while (regex_search(next, m, re)) {
+    VLOG(1) << "Start search " << m.size();
+    for (size_t i = 1; i < m.size(); ++i) {
+      auto& x = m[i];
+      if (x.matched && x.length() == 1) {
+        CHECK_EQ('\'', *x.first);
 
-template <> class RecordTraits<CastCrewObj> {
-  std::vector<char*> cols_;
+        VLOG(1) << "Found " << x.str();
 
- public:
-  static std::string Serialize(bool is_binary, const CastCrewObj& rec) {
-    return absl::StrCat(rec.movie_id);
+        char* val_ptr = next + (x.first - next);
+        *val_ptr++ = '"';
+        if (i == 3) {
+          CHECK(m[4].matched);
+          char* val_end = next + (m[4].second - next);
+          for (char* p = val_ptr; p != val_end; ++p) {
+            if (*p == '\\' && p[1] == '\'') {
+                *p = ' ';
+            } else if (*p == '"') {
+              *p = '\'';
+            }
+          }
+        }
+      }
+    }
+
+    next += (m.position() + m.length());
   }
 
-  bool Parse(bool is_binary, std::string&& tmp, CastCrewObj* res) {
-
-    return true;
+  // Remove hex escaping
+  re.assign(R"(": "([^"]*)\")");
+  next = b;
+  while (regex_search(next, m, re)) {
+    CHECK_EQ(2, m.size());
+    auto& x = m[1];
+    char* val_ptr = next + (x.first - next);
+    char* val_end = next + (x.second - next);
+    for (char* p = val_ptr; p != val_end; ++p) {
+      if (*p == '\\' && p[1] == 'x' && val_end - p > 3) {
+        memset(p, ' ', 4);
+      }
+    }
+    next += (m.position() + m.length());
   }
-};
-}  // namespace mr3
-*/
 
+  constexpr char kBadKey[] = R"("profile_path": None)";
+  constexpr size_t kBadLen = sizeof(kBadKey) - 1;
+
+  next = b;
+  while (true) {
+    next = strstr(next, kBadKey);
+    if (!next)
+      break;
+    memset(next, ' ', kBadLen);
+    next += kBadLen;
+  }
+}
 
 class CreditsMapper {
   std::vector<char*> cols_;
@@ -63,12 +103,11 @@ void CreditsMapper::Do(string val, mr3::DoContext<rj::Document>* context) {
   uint32_t movie_id;
 
   CHECK(absl::SimpleAtoi(cols_[2], &movie_id));
-  rj::Document cast;
-  cast.ParseInsitu<rj::kParseDefaultFlags>(cols_[0]);
 
-  bool has_error = cast.HasParseError();
-  LOG_IF(FATAL, has_error) << rj::GetParseError_En(cast.GetParseError()) << " for string "
-                           << cols_[0];
+  CleanBadJson(cols_[0], cols_[1]);
+  rj::Document doc;
+  bool has_error = doc.Parse<rj::kParseTrailingCommasFlag>(cols_[0]).HasParseError();
+  CHECK(!has_error) << rj::GetParseError_En(doc.GetParseError()) << cols_[0];
   // cast.AddMember("movie_id", rj::Value(movie_id), cast.GetAllocator());
 }
 
